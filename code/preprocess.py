38a00bf602f9e571183ae165d8c777a2e8045252
akshay <akshayk.vnit@gmail.com>
2019-07-15 14:35:00 +0200

    Partial code added for preprocessing and data_prep.

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,72 +1,48 @@
 import os
+import sys
 import csv
-import numpy as np
 
 # constant length of each example
-reqd_len = 150
+reqd_len = 50
 
-path = '../data/alternate_data.csv'
+path = '../data/new_data.csv'
 
 with open(path) as f :
     reader = csv.reader(f)
-    # empty list that will hold readings from one example
+    # empty list to hold readings from one example
     readings = list()
-    annotation = ''
-    for row in reader :
-        # if non-empty row, then append the row to readings
-        if row[0] != '' :
-            # add the accelerometer data only (not the annotation) since
-            # it will be easier to handle during finding the average of the
-            # readings for the padding values
-            readings.append(row[1 : ])
-            # print(row[1 : ])
-            # keep the label saved for using during padding
-            annotation = row[0]
 
-        # if empty row i.e. separation between 2 examples
-        # then current example has ended
-        if row[0] == '' :
-            # if less than 140 or more than 150 samples, then
-            # discard the reading
-            if len(readings) < 140 or len(readings) > 150 :
-                print('readings are not between 140 and 150 : ', len(readings))
+    time = 0
+    label = ''
+    for row in reader :
+        # if reading is to be continued i.e. both timestamp increases and label stays the same
+        if row[0] >= time and row[1] == label :
+            readings.append(row[2 : ])
+            annotation = row[1]
+            time = row[0]
+
+        # if timestamp value reduces, it means start of new example
+        # also, if label changes, it means start of new example
+        else :
+            # we need to integer divide the number of readings by `reqd_len`
+            # and then segment the data into those many examples
+            num_data = len(readings) // reqd_len
+            # if less than required length, discard the readings
+            if num_data == 0 :
+                print('readings are less than ', reqd_len)
+                # prepare for taking next block of data
                 readings = list()
+                label = row[1]
+                time = row[0]
                 continue
-            # Calculating amount of padding required
+
+            # calculating the amount of padding required
             length = len(readings)
             k = 0
-            # finding equal padding from both sides
-            pad_length = (reqd_len - length) // 2
-            # in case unsymmetrical padding is required
-            if ((pad_length * 2) + length) < reqd_len :
-                k = reqd_len - ((pad_length * 2) + length)
-                # print(k)
+            pad_length = (reqd_len * num_data) - length
+            # if too much padding is required, discard the excess readings
+            if pad_length > (num_data * 10) :
+                # TODO
 
-            print(length + (pad_length * 2) + k)
-
-            # converting each accelerometer reading into float from str
-            readings_ = np.array(readings, dtype = float)
-
-            # Determining the actual padding values
-            # Taking the average of the readings of that particular example
-            padding_ = (np.mean(readings_, axis = 0)).tolist()
-            # adding the annotation to the padding row (for consistent data)
-            padding_.append(annotation)
-            # print(len(padding_))
-
-            # writing the padding and data to csv
-            with open('../data/padded_data.csv', 'a') as wf :
-                wrf = csv.writer(wf)
-                for i in range(pad_length) :
-                    wrf.writerow(padding_)
-                for reading in readings :
-                    reading_ = reading
-                    # Adding the annotation to each row of the reading
-                    # since we had initially removed it
-                    reading_.append(annotation)
-                    wrf.writerow(reading_)
-                for i in range(pad_length + k) :
-                    wrf.writerow(padding_)
-
-            # make readings empty for next time
-            readings = list()
+            label = row[1]
+            time = row[0]
